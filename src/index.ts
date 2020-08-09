45bc5510310cf1fa46c508423111bdf498882dea
Fraser Tooth <ftooth@moneytree.jp>
2020-08-10 00:06:56 +0900

    added types

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,18 @@
 const lib = require("./library");
 // Lib has a simple (x,y) => {x+y} function called 'add'
 
-function map(array, callback) {
-  const output = [];
-
+function map(array: number[], callback: (x: number) => number): number[] {
+  const output: number[] = [];
   for (const item of array) {
     output.push(callback(item));
   }
-
   return output;
 }
 
 const input = [1, 2, 3, 4, 5];
 
-const result = map(input, (n) => {
-  lib.add(n, 10);
+const result = map(input, (n: number): number => {
+  return lib.add(n, 10);
 });
 
 console.log(result);
