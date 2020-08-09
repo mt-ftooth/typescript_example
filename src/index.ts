cb4375869df44cc474d8228d09fee28e98f52607
Fraser Tooth <ftooth@moneytree.jp>
2020-08-09 18:52:02 +0900

    Example

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
 const lib = require("./library");
+// Lib has a simple (x,y) => {x+y} function called 'add'
 
-console.log(lib.add(1, 2));
+function map(array, callback) {
+  const output = [];
+
+  for (const item of array) {
+    output.push(callback(item));
+  }
+
+  return output;
+}
+
+const input = [1, 2, 3, 4, 5];
+
+const result = map(input, (n) => {
+  lib.add(n, 10);
+});
+
+console.log(result);
